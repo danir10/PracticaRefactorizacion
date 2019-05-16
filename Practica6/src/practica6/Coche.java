b9a45da4c1a18184319dc00dc259dd252c6333cf
danir10 <depresetion45@gmail.com>
2019-05-16 13:39:41 +0200

    Clonar el repositorio y implementar los metodos.
    Cerramos los issues
    
    Closes #1 . Closes #5

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -19,18 +19,41 @@ public class Coche {
 		modelo = mo;
 		fabricante = f;
 	}
+	
+	
+	public String Gasolinacoche (String modelo, String fabricante)
+	{
+		//extraer variable
+		//duplicacion de codigo
+		String resultado =" es un "+ fabricante + modelo + "y gasta 1,337 euros por litro.";
+		return resultado;
+	}
+	
+
+	public String DieseCoche (String modelo, String fabricante)
+	{
+		String resultado =" es un "+ fabricante + modelo + "y gasta 1,052 euros por litro.";
+		return resultado;
+	}
+	
+	
+	public String HibridoCoche (String modelo, String fabricante)
+	{
+		String resultado =" es un "+ fabricante + modelo + "y no necesita combustible.";
+		return resultado;
+	}
 
 	public String metodo1() {
 		String resultado = "";
 		if (atrib == "Gasolina") {
 			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoA(modelo, fabricante);
+			resultado += Gasolinacoche(modelo, fabricante);
 		} else if (atrib == "Diesel") {
 			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoB(modelo, fabricante);
+			resultado += DieseCoche(modelo, fabricante);
 		} else if (atrib == "Híbrido") {
 			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoC(modelo, fabricante);
+			resultado += HibridoCoche(modelo, fabricante);
 		} else {
 			resultado += "El coche con matricula " + matricula + " ";
 			resultado += "no dispone de información";
